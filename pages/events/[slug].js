b45c65caf1a89db6f8f047f7e46fb81a974388e6
Utkarsh Tiwari <u.tiwari@huulke.com>
2021-05-26 10:46:22 +0530

    Layout, header and footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
+
 export default function EventPage() {
+  const router = useRouter()
+
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   )
 }
