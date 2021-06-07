7759b8671ede25e02782c7b18fe72157310f0f20
Utkarsh Tiwari <u.tiwari@huulke.com>
2021-06-07 19:04:17 +0530

    Add authorization

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
 
-export default function ImageUpload({ evtId, imageUploaded }) {
+export default function ImageUpload({ evtId, imageUploaded, token }) {
   const [image, setImage] = useState(null)
 
   const handleSubmit = async (e) => {
@@ -15,7 +15,10 @@ export default function ImageUpload({ evtId, imageUploaded }) {
 
     const res = await fetch(`${API_URL}/upload`, {
       method: 'POST',
-      body: formData
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
     })
 
     if (res.ok) {
--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -2,6 +2,7 @@ import moment from 'moment'
 import { FaImage } from 'react-icons/fa'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { parseCookies } from '@/helpers/index'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
@@ -12,7 +13,7 @@ import ImageUpload from '@/components/ImageUpload'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
 
-export default function EditEventPage({ evt }) {
+export default function EditEventPage({ evt, token }) {
   const [values, setValues] = useState({
     name: evt.name,
     performers: evt.performers,
@@ -42,12 +43,17 @@ export default function EditEventPage({ evt }) {
       const res = await fetch(`${API_URL}/events/${evt.id}`, {
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
         },
         body: JSON.stringify(values)
       })
 
       if (!res.ok) {
+        if (res.status === 403 || res.status === 401) {
+          toast.error('Unauthorized')
+          return
+        }
         toast.error('Something Went Wrong')
       } else {
         const evt = await res.json()
@@ -168,19 +174,26 @@ export default function EditEventPage({ evt }) {
       </div>
 
       <Modal show={showModal} onClose={() => setShowModel(false)}>
-        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+        <ImageUpload
+          evtId={evt.id}
+          imageUploaded={imageUploaded}
+          token={token}
+        />
       </Modal>
     </Layout>
   )
 }
 
 export async function getServerSideProps({ params: { id }, req }) {
+  const { token } = parseCookies(req)
+
   const res = await fetch(`${API_URL}/events/${id}`)
   const evt = await res.json()
 
   return {
     props: {
-      evt
+      evt,
+      token
     }
   }
 }
